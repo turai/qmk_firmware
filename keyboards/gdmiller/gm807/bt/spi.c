4263f52716dfa1ce5f608d5364f2027fb04ac5eb
1Conan <me@1conan.com>
2022-01-31 14:55:11 +0800

    add sn32f260 to iton bt

--- keyboards/gdmiller/gm807/bt/spi.c
+++ keyboards/gdmiller/gm807/bt/spi.c
@@ -13,33 +13,45 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifdef BT_SN32F260
+#include <SN32F260.h>
+#else
 #include <SN32F240B.h>
+#endif
+
 #include <stdbool.h>
 #include "spi.h"
 #include "hal.h"
 #include "quantum.h"
 
+#define SPI SN_SPI0
+#define SYS SN_SYS1
+
 void spi_init() {
-    SN_SYS1->AHBCLKEN_b.SPI0CLKEN   = true;
-    SN_SPI0->CTRL0_b.DL             = SPI_DL_8;
-    SN_SPI0->CTRL0_b.MS             = SPI_MS_SLAVE_MODE;
-    SN_SPI0->CTRL0_b.LOOPBACK       = SPI_LOOPBACK_DIS;
-    SN_SPI0->CTRL0_b.SDODIS         = SPI_SDODIS_EN;
-    SN_SPI0->CTRL0_b.RXFIFOTH       = SPI_RXFIFOTH_7;
-    SN_SPI0->CTRL0_b.TXFIFOTH       = SPI_TXFIFOTH_7;
+    SYS->AHBCLKEN_b.SPI0CLKEN   = true;
+    SPI->CTRL0_b.DL             = SPI_DL_8;
+    SPI->CTRL0_b.MS             = SPI_MS_SLAVE_MODE;
+    SPI->CTRL0_b.LOOPBACK       = SPI_LOOPBACK_DIS;
+    SPI->CTRL0_b.SDODIS         = SPI_SDODIS_EN;
+    SPI->CTRL0_b.RXFIFOTH       = SPI_RXFIFOTH_7;
+    SPI->CTRL0_b.TXFIFOTH       = SPI_TXFIFOTH_7;
 
-    SN_SPI0->CLKDIV_b.DIV           = (SPI_DIV / 2) - 1; // 2
-    SN_SPI0->CTRL1_b.CPHA           = SPI_CPHA_FALLING_EDGE;
-    SN_SPI0->CTRL1_b.CPOL           = SPI_CPOL_SCK_IDLE_LOW;
-    SN_SPI0->CTRL1_b.MLSB           = SPI_MLSB_MSB;
-    SN_SPI0->CTRL0_b.SELDIS         = SPI_SELDIS_EN;
+    SPI->CLKDIV_b.DIV           = (SPI_DIV / 2) - 1; // 2
+    SPI->CTRL1_b.CPHA           = SPI_CPHA_FALLING_EDGE;
+    SPI->CTRL1_b.CPOL           = SPI_CPOL_SCK_IDLE_LOW;
+    SPI->CTRL1_b.MLSB           = SPI_MLSB_MSB;
+    SPI->CTRL0_b.SELDIS         = SPI_SELDIS_EN;
 
-    SN_SPI0->CTRL0_b.FRESET         = SPI_FRESET_RESET_FIFO; // SPI0_FIFO_RESET
+    SPI->CTRL0_b.FRESET         = SPI_FRESET_RESET_FIFO; // SPI0_FIFO_RESET
 
 	NVIC_DisableIRQ(SPI0_IRQn);
     writePinLow(SPI_IRQ_PIN);
 
-    SN_SPI0->CTRL0_b.SPIEN = SPI_SPIEN_EN;
+#ifdef BT_SN32F260
+    SPI->CTRL0_b.SSPEN = SPI_SPIEN_EN;
+#else
+    SPI->CTRL0_b.SPIEN = SPI_SPIEN_EN;
+#endif
 }
 
 void spi_send_init(uint8_t _len) {
@@ -47,8 +59,8 @@ void spi_send_init(uint8_t _len) {
 }
 
 void spi_send_byte(uint8_t data) {
-    while (SN_SPI0->STAT_b.TX_FULL);
-    SN_SPI0->DATA = data;
+    while (SPI->STAT_b.TX_FULL);
+    SPI->DATA = data;
 }
 
 void spi_send_bytes(uint8_t *data, uint8_t len) {
@@ -58,6 +70,6 @@ void spi_send_bytes(uint8_t *data, uint8_t len) {
 }
 
 void spi_send_end() {
-    while (!SN_SPI0->STAT_b.TX_EMPTY);
+    while (!SPI->STAT_b.TX_EMPTY);
     writePinLow(SPI_IRQ_PIN);
 }
