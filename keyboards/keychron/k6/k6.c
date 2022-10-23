6df48469f79401b9da8ee3f8625fb7653276f4c1
1Conan <me@1conan.com>
2022-10-24 02:32:44 +0800

    add missing keycode handling

--- keyboards/keychron/k6/k6.c
+++ keyboards/keychron/k6/k6.c
@@ -15,3 +15,49 @@
  */
 
 #include "k6.h"
+#ifdef BLUETOOTH_ENABLE
+#include "outputselect.h"
+#include "iton_bt.h"
+#endif
+
+void keyboard_pre_init_kb(void)  {
+
+#ifdef BLUETOOTH_ENABLE
+    iton_bt_start();
+#endif
+
+    keyboard_pre_init_user();
+}
+
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch(keycode) {
+#ifdef BLUETOOTH_ENABLE
+            case BT_MODE_TOGGLE:
+                uint8_t where = where_to_send();
+                if (where == OUTPUT_BLUETOOTH) {
+                    set_output(OUTPUT_USB);
+                } else if (where == OUTPUT_USB) {
+                    set_output(OUTPUT_BLUETOOTH);
+                }
+                break;
+            case BT_PROFILE1:
+                iton_bt_switch_profile(0);
+                break;
+            case BT_PROFILE2:
+                iton_bt_switch_profile(1);
+                break;
+            case BT_PROFILE3:
+                iton_bt_switch_profile(2);
+                break;
+            case BT_PAIR:
+                iton_bt_enter_pairing();
+                break;
+#endif
+            default:
+                break;
+        }
+    }
+    return process_record_user(keycode, record);
+}
