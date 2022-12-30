437640d05467b1ce5ca5c054f6723f0f0947ea7c
1Conan <me@1conan.com>
2022-12-30 20:04:44 +0800

    fix k5

--- keyboards/keychron/k5/rgb/rgb.h
+++ keyboards/keychron/k5/rgb/rgb.h
@@ -18,12 +18,12 @@
 
 #include "quantum.h"
 
-#if defined(KEYBOARD_keychron_k2_rgb_ansi)
+#if defined(KEYBOARD_keychron_k5_rgb_ansi)
     #include "ansi.h"
-#elif defined(KEYBOARD_keychron_k2_rgb_iso)
+#elif defined(KEYBOARD_keychron_k5_rgb_iso)
     #include "iso.h"
-#elif defined(KEYBOARD_keychron_k2_rgb_optical_ansi)
+#elif defined(KEYBOARD_keychron_k5_rgb_optical_ansi)
     #include "optical_ansi.h"
-#elif defined(KEYBOARD_keychron_k2_rgb_optical_iso)
+#elif defined(KEYBOARD_keychron_k5_rgb_optical_iso)
     #include "optical_iso.h"
 #endif
