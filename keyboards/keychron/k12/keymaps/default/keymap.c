7ffed9a6034ca5db7d8855e5bc7c2ea198bb696f
turai <turai@turai.xyz>
2024-08-10 13:22:06 +0200

    Oops

--- keyboards/keychron/k12/keymaps/default/keymap.c
+++ keyboards/keychron/k12/keymaps/default/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│GUI │Alt │                        │ Alt│Fn1 │Fn2 │Ctrl│
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
-    [WIN_BASE] = LAYOUT_ansi_60(
+    [WIN_BASE] = LAYOUT_60_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,   KC_BSPC, 
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,     KC_RBRC,  KC_BSLS, 
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_ENT,  
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │    │    │    │                        │    │    │    │    │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
-    [WIN_FN1] = LAYOUT_ansi_60(
+    [WIN_FN1] = LAYOUT_60_ansi(
         _______, KC_BRIU, KC_BRID, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, RGB_MOD,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_APP,  KC_SCRL, KC_INS,  KC_PGUP, KC_HOME, _______, 
         _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   KC_PSCR, KC_PGDN, KC_END,           _______,
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│Alt │GUI │                        │ GUI│Fn1 │Fn2 │Ctrl│
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
-    [MAC_BASE] = LAYOUT_ansi_60(
+    [MAC_BASE] = LAYOUT_60_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,   KC_BSPC, 
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,     KC_RBRC,  KC_BSLS, 
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_ENT,  
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │    │    │    │                        │    │    │    │    │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
-    [MAC_FN1] = LAYOUT_ansi_60(
+    [MAC_FN1] = LAYOUT_60_ansi(
         _______, KC_BRIU, KC_BRID, KC_MSSN, KC_FIND, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, RGB_MOD,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_APP,  KC_SCRL, KC_INS,  KC_PGUP, KC_HOME, _______, 
         _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   KC_MSNP, KC_PGDN, KC_END,           _______,
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │    │    │    │                        │    │    │    │    │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
      */
-    [FN2] = LAYOUT_ansi_60(
+    [FN2] = LAYOUT_60_ansi(
         Q_RESET, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RGB_TOG,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
